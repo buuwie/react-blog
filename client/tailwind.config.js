71322fc8bc52e52fe07053664d7a1a4aff3d0b05
buuwie <hungtranngoc2002@gmail.com>
2024-04-08 13:05:37 +0700

    header

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,26 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'lobster': ['Lobster'],
+    },
+    colors: {
+      buttextdark: '#05f7ff',
+      buttextlight: '#009fcb',
+      kechuyentextlight: '#cccccc',
+      default: '#374151',
+    },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
 
